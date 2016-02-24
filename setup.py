089d69e7bd3e6da870f0badf683283574dd5f4c0
Gavin Cooper <Gavin.Cooper@newcastle.edu.au>
2016-02-24 15:15:32 +1100

    Rename project and update setup

--- setup.py
+++ setup.py
@@ -3,14 +3,14 @@ from setuptools import setup
 with open('README.md') as rfile:
     long_description = rfile.read()
 
-setup(name='pypres',
+setup(name='prespy',
       version='0.0.1',
       description='Package for working with the Neurobehavioural Systems' +
                   ' Presentation logfiles within python',
       long_description=long_description,
-      url='https://github.com/gjcooper/eegevt',
+      url='https://github.com/gjcooper/prespy',
       author='Gavin Cooper',
       author_email='gjcooper@gmail.com',
       license='GPL v2.0',
-      packages=['pypres'],
+      packages=['prespy'],
       keywords=['Presentation', 'NBS', 'logfile'])
