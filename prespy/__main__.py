fdfea3b67c2ab8aeaf69a9f9214afc814db0c985
gjcooper <gjcooper@gmail.com>
2017-03-15 17:14:20 +1100

    Fix error on arguments, cast to correct type

--- prespy/__main__.py
+++ prespy/__main__.py
@@ -7,9 +7,10 @@ def scla():
     parser.add_argument('--runid', '-i', help='An id for the sound plot', default='scla')
     parser.add_argument('soundfile', help='A sound recording of port and sound output')
     parser.add_argument('logfile', help='Stimulus delivery recording of event sequence')
-    parser.add_argument('--schannel', '-c', help='The channel sounds were recorded in', default=1)
-    parser.add_argument('--maxdur', '-m', help='Max duration for sound/port event', default=0.012)
-    parser.add_argument('--thresh', '-t', help='Threshold for sound/port detection', default=0.2)
+    parser.add_argument('--schannel', '-c', help='The channel sounds were recorded in', default=1, type=int)
+    parser.add_argument('--maxdur', '-m', help='Max duration for sound/port event', default=0.012, type=float)
+    parser.add_argument('--thresh', '-t', help='Threshold for sound/port detection', default=0.2, type=float)
+
 
     args = parser.parse_args()
 
