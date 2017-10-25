ec1f1b18e13682e9e2fdf09f88d35a522f4edc19
gjcooper <gjcooper@gmail.com>
2017-10-25 16:23:15 +1100

    Split duration/threshold values into separate channels

--- src/prespy/sndan.py
+++ src/prespy/sndan.py
@@ -45,7 +45,24 @@ def extract_channel_events(channel, maxdur=0.012, thresh=0.2, samplerate=44100):
     return events
 
 
-def extract_sound_events(soundfile, schannel=1, maxdur=0.012, thresh=0.2, plot=False, runid='scla'):
+def _fix_for_sigchange(kwargs):
+    '''Populate with channel specific key,vals when called with non specific versions'''
+    if 'maxdur' in kwargs:
+        if 'snddur' in kwargs or 'portdur' in kwargs:
+            raise RuntimeError('maxdur argument in arguments AND either snddur or portdur in arguments')
+        kwargs['snddur'] = kwargs['maxdur']
+        kwargs['portdur'] = kwargs['maxdur']
+        warnings.warn('maxdur argument is deprecated and will be removed in a future release', DeprecationWarning)
+    if 'thresh' in kwargs:
+        if 'sndthresh' in kwargs or 'portthresh' in kwargs:
+            raise RuntimeError('thresh argument in arguments AND either sndthresh or portthresh in arguments')
+        kwargs['sndthresh'] = kwargs['thresh']
+        kwargs['portthresh'] = kwargs['thresh']
+        warnings.warn('thresh argument is deprecated and will be removed in a future release', DeprecationWarning)
+    return kwargs
+
+
+def extract_sound_events(soundfile, schannel=1, plot=False, runid='scla', **kwargs):
     fs, sdata = wavLoad(soundfile)  # wavfile.read(soundfile)
     # Grab channels
     port = sdata[1 - schannel]
@@ -55,14 +72,20 @@ def extract_sound_events(soundfile, schannel=1, maxdur=0.012, thresh=0.2, plot=F
     maxs = max(snd)
     port = [p / maxp for p in port]
     snd = [s / maxs for s in snd]
-    port_events = extract_channel_events(port, maxdur=maxdur, thresh=thresh, samplerate=fs)
-    snd_events = extract_channel_events(snd, maxdur=maxdur, thresh=thresh, samplerate=fs)
+    port_events = extract_channel_events(port,
+                                         maxdur=kwargs['portdur'],
+                                         thresh=kwargs['portthresh'],
+                                         samplerate=fs)
+    snd_events = extract_channel_events(snd,
+                                        maxdur=kwargs['snddur'],
+                                        thresh=kwargs['sndthresh'],
+                                        samplerate=fs)
     if plot:
         try:
             import matplotlib.pyplot as plt
         except ImportError:
             print('matplotlib not found, skipping plot of first sound')
-        plt.plot(snd[int(snd_events[0] * fs) - 100:int((snd_events[0] + maxdur) * fs)])
+        plt.plot(snd[int(snd_events[0] * fs) - 100:int((snd_events[0] + kwargs['snddur']) * fs)])
         plt.savefig(runid + '_firstsnd.png')
         plt.close()
     return fs, port_events, snd_events, port
@@ -71,6 +94,8 @@ def extract_sound_events(soundfile, schannel=1, maxdur=0.012, thresh=0.2, plot=F
 def scla(soundfile=None, logfile=None, **kwargs):
     """Implements similar logic to Neurobehavioural Systems SCLA program"""
     log = load(logfile)
+    # Fix for previous call signatures still in use
+    kwargs = _fix_for_sigchange(kwargs)
 
     fs, pcodes, snds, port = extract_sound_events(soundfile, **kwargs)
     if (len(log.events) != len(pcodes)) or (len(pcodes) != len(snds)):
@@ -89,18 +114,21 @@ def scla(soundfile=None, logfile=None, **kwargs):
     return stdStats(thisdata)
 
 
-def timing(port, pcodes, snds, fs, maxdur=0, thresh=0, **kwargs):
+def timing(port, pcodes, snds, fs, portdur=0, portthresh=0, **kwargs):
     """extract extra info about port duration and time between stimuli"""
     timediffs = {'pcodes': [], 'snds': []}
     portlengths = []
+    # Port evt to Port evt in time
     for p in range(1, len(pcodes)):
         timediffs['pcodes'].append(pcodes[p] - pcodes[p - 1])
+    # Sound evt to Sound evt in time
     for s in range(1, len(snds)):
         timediffs['snds'].append(snds[s] - snds[s - 1])
+    # Length of each port event
     for p in pcodes:
-        span = range(int(p * fs) + 1, int((p + maxdur * 1000) * fs))
+        span = range(int(p * fs) + 1, int((p + portdur * 1000) * fs))
         for pt in span:
-            if port[pt] < thresh:
+            if not _exceeds_threshold(port[pt], portthresh):
                 portlengths.append(pt / fs - p)
                 break
     return timediffs, portlengths
